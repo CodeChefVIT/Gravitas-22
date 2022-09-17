accd864619d6b8f831ef709c8df9e5fad6413fc1
SayarB <sayarbhattacharyya03@gmail.com>
2022-09-17 14:57:09 +0530

    feat: tracking the focus and updating the navbar

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-export default function Navbar() {
+import { useEffect, useState } from "react";
+export default function Navbar({ focus }) {
   return (
     <div className="fixed z-10 bg-white top-0 left-0 w-[100vw] h-[12vh] border-b-2 border-black flex items-center">
       <div className="border-black border-r-2 h-full items-center justify-center aspect-square hidden sm:flex">
@@ -10,11 +11,43 @@ export default function Navbar() {
       </div>
 
       <div className="h-full font-mudclaw flex-1 flex items-center justify-evenly text-sm md:text-lg">
-        <Link href={"#banner"}>HOME</Link>
-        <Link href={"#events"}>EVENTS</Link>
-        <Link href={"#events"}>SPONSORS</Link>
+        <div
+          style={{
+            border: focus === "banner" && "1px solid orange",
+            padding: focus === "banner" ? "10px" : "0",
+          }}
+          className="rounded-[50%] transition-all"
+        >
+          <Link href={"#banner"}>HOME</Link>
+        </div>
+        <div
+          style={{
+            border: focus === "events" && "1px solid orange",
+            padding: focus === "events" ? "10px" : "0",
+          }}
+          className="rounded-[50%] transition-all"
+        >
+          <Link href={"#events"}>EVENTS</Link>
+        </div>
+        <div
+          style={{
+            border: focus === "sponsors" && "1px solid orange",
+            padding: focus === "sponsors" ? "10px" : "0",
+          }}
+          className="rounded-[50%] transition-all"
+        >
+          <Link href={"#sponsors"}>SPONSORS</Link>
+        </div>
 
-        <Link href={"#"}>CONTACT</Link>
+        <div
+          style={{
+            border: focus === "footer" && "1px solid orange",
+            padding: focus === "footer" ? "10px" : "0",
+          }}
+          className="rounded-[50%] transition-all"
+        >
+          <Link href={"#footer"}>CONTACT</Link>
+        </div>
       </div>
       <div className="h-[70%] w-[15vw] hidden md:flex items-center justify-center relative mr-5">
         <Image
--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import Navbar from "../components/Navbar/Navbar";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Navbar />
       <Component {...pageProps} />
     </>
   );
