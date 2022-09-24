47bacb599802dcad5ddc4dbdeff0d7e12d695f14
SayarB <sayarbhattacharyya03@gmail.com>
2022-09-24 21:43:15 +0530

    fix: minor fixes

--- pages/index.js
+++ pages/index.js
@@ -100,7 +100,11 @@ export default function Home() {
           <h1 className="text-3xl font-mudclaw text-[#FDC62E] text-center">
             Our Sponsors
           </h1>
-          <div className="h-[70vh]"></div>
+          <div className="h-[70vh] flex items-center justify-center">
+            <p className="text-xl md:text-3xl font-mudclaw">
+              To Be Announced ...
+            </p>
+          </div>
         </section>
         <footer
           className=" text-sm lg:text-base font-mabry h-fit bg-[#B7DCEF]"
@@ -113,13 +117,13 @@ export default function Home() {
           <div className="m-auto max-w-[1900px] w-fit h-full flex flex-col justify-end">
             <div className=" w-[90%] mx-auto flex flex-col items-center justify-center md:flex-row border-b-[1px] border-black">
               <p className="mb-5 mt-20 w-[100%] whitespace-normal  flex-1 text-justify">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
+                CodeChef-VIT is a prominent technical chapter at VIT Vellore
+                with a goal to provide students with a platform for programmers
+                and developers everywhere to learn and grow alongside one
+                another. We also promote the all-round development of students
+                through regular events such as workshops, projects showcases and
+                other interactive sessions that can help them perceive
+                real-world problems.
               </p>
               <div className=" flex-[0.3] mt-12 mx-10 mb-5 grid  items-center grid-cols-3 lg:flex">
                 <SocialLink icon={"/twitter.svg"} bgColor="#23BA69" />
